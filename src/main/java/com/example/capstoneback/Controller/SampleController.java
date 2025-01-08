88799dca0bf42b32d7ee3279207ded317b18499d
cubric0223 <cubric0223@gmail.com>
2025-01-08 15:45:10 +0900

    second

--- src/main/java/com/example/capstoneback/Controller/SampleController.java
+++ src/main/java/com/example/capstoneback/Controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
+    // 기본 페이지
     @GetMapping("/")
     public String sampleMapping(){
         return "sample";
