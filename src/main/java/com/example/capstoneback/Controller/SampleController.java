d21920e20f398155dd64fd6bd5c2768a954a193f
juhanPark <iiiamst16@gmail.com>
2025-01-08 15:01:22 +0900

    test commit

--- src/main/java/com/example/capstoneback/Controller/SampleController.java
+++ src/main/java/com/example/capstoneback/Controller/SampleController.java
@@ -2,11 +2,18 @@ package com.example.capstoneback.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
     @GetMapping("/")
     public String sampleMapping(){
         return "sample";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
     }
 }
