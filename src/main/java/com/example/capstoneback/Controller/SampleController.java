e96e484a224698bd897605812552a8beb8b7f89a
cubric0223 <cubric0223@gmail.com>
2025-01-08 15:59:17 +0900

    web hook test 3

--- src/main/java/com/example/capstoneback/Controller/SampleController.java
+++ src/main/java/com/example/capstoneback/Controller/SampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
-    // 기본 페이지 web hook test 2
+    // 기본 페이지 web hook test 3
     @GetMapping("/")
     public String sampleMapping(){
         return "sample";
