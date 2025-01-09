c3eb74702f71861a2f8bc4cc76af4c3670031a6f
cubric0223 <cubric0223@gmail.com>
2025-01-09 15:15:12 +0900

    test2

--- src/main/java/com/example/capstoneback/Controller/SampleController.java
+++ src/main/java/com/example/capstoneback/Controller/SampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
-    // cd test 1
+    // cd test 2
     @GetMapping("/")
     public String sampleMapping(){
         return "sample";
