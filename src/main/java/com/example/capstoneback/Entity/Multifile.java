af56581b9c00a63167983f4394de3f5a925f4750
JuHan Park <iiiamst16@gmail.com>
2025-03-06 16:14:36 +0900

    feat: Multifile 엔티티 생성 및 이메일에 일대다 필드 추가

---
+++ src/main/java/com/example/capstoneback/Entity/Multifile.java
@@ -0,0 +1,26 @@
+package com.example.capstoneback.Entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Multifile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "file_name", nullable = false)
+    private String fileName;
+
+    @Column(name = "file_path", nullable = false)
+    private String filePath;
+
+    @ManyToOne
+    @JoinColumn
+    private Email email;
+}
