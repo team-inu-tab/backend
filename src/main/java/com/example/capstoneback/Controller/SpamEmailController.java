20489fe6cb941cc42ec96e3d8c4b88a51e7a3961
cubric0223 <cubric0223@gmail.com>
2025-03-28 16:14:33 +0900

    스팸 차단, 스팸 차단 해제, 스팸메일함 조회

--- src/main/java/com/example/capstoneback/Controller/SpamEmailController.java
+++ src/main/java/com/example/capstoneback/Controller/SpamEmailController.java
@@ -1,4 +1,59 @@
 package com.example.capstoneback.Controller;
 
+import com.example.capstoneback.DTO.ReceivedEmailResponseDTO;
+import com.example.capstoneback.Service.SpamEmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/mails/spam")
+@RequiredArgsConstructor
 public class SpamEmailController {
+
+    private final SpamEmailService spamEmailService;
+
+    // 스팸 메일함으로 이동
+    @PostMapping("/{messageId}")
+    public ResponseEntity<String> addToSpam(@PathVariable String messageId, Authentication authentication) {
+        try {
+            spamEmailService.addToSpam(messageId, authentication);
+            return ResponseEntity.ok("스팸메일함 이동 성공");
+        } catch (IOException e) {
+            System.out.println(e);
+            return ResponseEntity
+                    .internalServerError()
+                    .body("스팸메일 이동 실패: " + e.getMessage());
+        }
+    }
+
+    // 스팸 해제 (받은 편지함으로 복원)
+    @DeleteMapping("/{messageId}")
+    public ResponseEntity<String> removeFromSpam(@PathVariable String messageId, Authentication authentication) {
+        try {
+            spamEmailService.removeFromSpam(messageId, authentication);
+            return ResponseEntity.ok("메일이 받은편지함으로 복원되었습니다.");
+        } catch (IOException e) {
+            System.out.println(e);
+            return ResponseEntity
+                    .internalServerError()
+                    .body("스팸메일 해제 실패: " + e.getMessage());
+        }
+    }
+
+    // 스팸 메일함 목록 조회
+    @GetMapping
+    public ResponseEntity<List<ReceivedEmailResponseDTO>> getSpamEmails(Authentication authentication) {
+        try {
+            List<ReceivedEmailResponseDTO> emails = spamEmailService.getSpamEmails(authentication);
+            return ResponseEntity.ok(emails);
+        } catch (IOException e) {
+            System.out.println(e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
--- src/main/java/com/example/capstoneback/Service/SpamEmailService.java
+++ src/main/java/com/example/capstoneback/Service/SpamEmailService.java
@@ -1,25 +1,136 @@
 package com.example.capstoneback.Service;
 
+import com.example.capstoneback.DTO.ReceivedEmailResponseDTO;
+import com.example.capstoneback.Entity.User;
+import com.example.capstoneback.Error.ErrorCode;
+import com.example.capstoneback.Error.UserDoesntExistException;
+import com.example.capstoneback.Repository.UserRepository;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.model.ModifyMessageRequest;
+import com.google.api.services.gmail.model.*;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.Collections;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Service
+@RequiredArgsConstructor
 public class SpamEmailService {
-    private final Gmail gmail;
 
-    public SpamEmailService(Gmail gmail) {
-        this.gmail = gmail;
+    private final UserRepository userRepository;
+
+    //Http 요청 객체와 Json 변환 객체 생성
+    private static final HttpTransport httpTransport = new NetHttpTransport();
+    private static final JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+    // 스팸 추가
+    public void addToSpam(String messageId, Authentication authentication) throws IOException {
+        User user = getUserFromAuth(authentication);
+        Gmail gmail = getGmailService(user);
+
+        // gmail api에서 메일에 라벨을 추가 or 제거
+        ModifyMessageRequest request = new ModifyMessageRequest()
+                .setAddLabelIds(List.of("SPAM"))
+                .setRemoveLabelIds(List.of("INBOX"));
+
+        gmail.users().messages().modify(user.getEmail(), messageId, request).execute();
     }
 
-    public void markMessageAsSpam(String userId, String messageId) throws IOException {
-        ModifyMessageRequest mods = new ModifyMessageRequest()
-                .setAddLabelIds(Collections.singletonList("SPAM"));
+    // 스팸 해제
+    public void removeFromSpam(String messageId, Authentication authentication) throws IOException {
+        User user = getUserFromAuth(authentication);
+        Gmail gmail = getGmailService(user);
 
-        gmail.users().messages().modify(userId, messageId, mods).execute();
+        // gmail api에서 메일에 라벨을 추가 or 제거
+        ModifyMessageRequest request = new ModifyMessageRequest()
+                .setAddLabelIds(List.of("INBOX"))
+                .setRemoveLabelIds(List.of("SPAM"));
+
+        gmail.users().messages().modify(user.getEmail(), messageId, request).execute();
+    }
+
+    // 스팸 목록 조회
+    public List<ReceivedEmailResponseDTO> getSpamEmails(Authentication authentication) throws IOException {
+        User user = getUserFromAuth(authentication);
+        Gmail gmail = getGmailService(user);
+
+        List<Message> spamMessages = gmail.users().messages().list(user.getEmail())
+                .setLabelIds(List.of("SPAM"))
+                .setMaxResults(10L)
+                .execute()
+                .getMessages();
+
+        List<ReceivedEmailResponseDTO> responseList = new ArrayList<>();
+
+        for (Message msg : spamMessages) {
+            Message detail = gmail.users().messages().get(user.getEmail(), msg.getId()).execute();
+            responseList.add(toReceivedEmailDTO(detail));
+        }
+
+        return responseList;
+    }
+
+    // 유저 조회
+    private User getUserFromAuth(Authentication authentication) {
+        return userRepository.findByUsername(authentication.getName())
+                .orElseThrow(() -> new UserDoesntExistException(ErrorCode.USER_DOESNT_EXIST));
+    }
+
+    // Gmail 서비스 생성
+    private Gmail getGmailService(User user) {
+        GoogleCredentials credentials = GoogleCredentials.create(new AccessToken(user.getAccessToken(), null));
+        return new Gmail.Builder(httpTransport, jsonFactory, new HttpCredentialsAdapter(credentials))
+                .setApplicationName("maeil-mail")
+                .build();
+    }
+
+    // DTO 변환
+    private ReceivedEmailResponseDTO toReceivedEmailDTO(Message message) {
+        String title = null, from = null;
+        LocalDateTime date = null;
+
+        for (MessagePartHeader header : message.getPayload().getHeaders()) {
+            switch (header.getName()) {
+                case "Subject": title = header.getValue(); break;
+                case "From": from = header.getValue(); break;
+                case "Date":
+                    String dateStr = header.getValue().replace(" (UTC)", "").replace(" (GMT)", "");
+                    ZonedDateTime zdt = ZonedDateTime.parse(dateStr, DateTimeFormatter.RFC_1123_DATE_TIME);
+                    date = zdt.withZoneSameInstant(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+                    break;
+            }
+        }
+
+        List<HashMap<String, String>> attachments = new ArrayList<>();
+        if (message.getPayload().getParts() != null) {
+            for (MessagePart part : message.getPayload().getParts()) {
+                if (!part.getFilename().isEmpty()) {
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put("fileName", part.getFilename());
+                    map.put("attachmentId", part.getBody().getAttachmentId());
+                    attachments.add(map);
+                }
+            }
+        }
+
+        return ReceivedEmailResponseDTO.builder()
+                .id(message.getId())
+                .title(title)
+                .content(message.getSnippet())
+                .sender(from)
+                .receiveAt(date)
+                .isImportant(message.getLabelIds().contains("STARRED"))
+                .fileNameList(attachments)
+                .build();
     }
 }
