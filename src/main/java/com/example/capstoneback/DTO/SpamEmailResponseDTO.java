582d06174a72a2556abee5414071b7e746735950
Juhan Park <iiiamst16@gmail.com>
2025-04-03 17:52:19 +0900

    fix: 스팸메일DTO receiver -> sender 수정

--- src/main/java/com/example/capstoneback/DTO/SpamEmailResponseDTO.java
+++ src/main/java/com/example/capstoneback/DTO/SpamEmailResponseDTO.java
@@ -12,17 +12,17 @@ public class SpamEmailResponseDTO {
     private String id;
     private String title;
     private String content;
-    private String receiver;
+    private String sender;
     private LocalDateTime receiveAt;
     private Boolean isImportant;
     private List<HashMap<String, String>> fileNameList;
 
     @Builder
-    public SpamEmailResponseDTO(String id, String title, String content, String receiver, LocalDateTime receiveAt, Boolean isImportant, List<HashMap<String, String>> fileNameList) {
+    public SpamEmailResponseDTO(String id, String title, String content, String sender, LocalDateTime receiveAt, Boolean isImportant, List<HashMap<String, String>> fileNameList) {
         this.id = id;
         this.title = title;
         this.content = content;
-        this.receiver = receiver;
+        this.receiver = sender;
         this.receiveAt = receiveAt;
         this.isImportant = isImportant;
         this.fileNameList = fileNameList;
