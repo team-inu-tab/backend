5b7855e31fac78547e5c424fac4db38de391d377
cubric0223 <cubric0223@gmail.com>
2025-04-03 16:43:23 +0900

    fixed spam mail

--- src/main/java/com/example/capstoneback/Service/GmailService.java
+++ src/main/java/com/example/capstoneback/Service/GmailService.java
@@ -391,7 +391,14 @@ public class GmailService {
                     .isImportant(detail.getLabelIds().contains("STARRED"))
                     .fileNameList(attachments).build());
         }
-        
+
+        if (dtoClass == SpamEmailResponseDTO.class) {
+            return dtoClass.cast(SpamEmailResponseDTO.builder()
+                    .id(message.getId()).title(headers.get("Subject"))
+                    .receiver(headers.get("To")).content(detail.getSnippet())
+                    .receiveAt(date).isImportant(detail.getLabelIds().contains("STARRED"))
+                    .fileNameList(attachments).build());
+        }
         throw new IllegalArgumentException("Unsupported DTO Type");
     }
     
